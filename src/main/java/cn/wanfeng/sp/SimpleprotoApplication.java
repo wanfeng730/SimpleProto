3c79fbd2083ea54fa2a9ab69110aa434c327987e
luozh <luozh@amberdata.cn>
2024-06-19 15:46:53 +0800

    init simpleproto

---
+++ src/main/java/cn/wanfeng/sp/SimpleprotoApplication.java
@@ -0,0 +1,13 @@
+package cn.wanfeng.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleprotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleprotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wanfeng/sp/SimpleprotoApplicationTests.java
@@ -0,0 +1,10 @@
+package cn.wanfeng.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleprotoApplicationTests {
+
+
+}
