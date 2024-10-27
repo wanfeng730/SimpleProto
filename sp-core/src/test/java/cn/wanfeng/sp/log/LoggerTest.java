13ae25d38e38faf8a70305fd04c7900afd1cf882
wanfeng730 <2580330397@qq.com>
2024-10-27 17:14:07 +0800

    feat SpBaseObject基础对象开发 1.日志配置更新

--- sp-core/src/test/java/cn/wanfeng/sp/log/LoggerTest.java
+++ sp-core/src/test/java/cn/wanfeng/sp/log/LoggerTest.java
@@ -1,14 +1,17 @@
 package cn.wanfeng.sp.log;
 
+import cn.wanfeng.sp.SimpleprotoApplication;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @date: 2024-08-31 14:16
  * @author: luozh
  * @description:
  */
+@SpringBootTest(classes = SimpleprotoApplication.class)
 public class LoggerTest {
 
     @Test
