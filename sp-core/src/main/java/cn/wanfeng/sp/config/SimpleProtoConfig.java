e89309eab9b1fd80034a7aaa89e12e5f189a3a10
wanfeng730 <2580330397@qq.com>
2024-11-06 02:53:53 +0800

    feat simpleproto配置支持多种类型数据源

--- sp-core/src/main/java/cn/wanfeng/sp/config/SimpleProtoConfig.java
+++ sp-core/src/main/java/cn/wanfeng/sp/config/SimpleProtoConfig.java
@@ -19,14 +19,14 @@ public class SimpleProtoConfig {
     public static String dataTable;
     public static String settingsTable;
 
-    public static String postgresHost;
-    public static String postgresPort;
-    public static String postgresDatabase;
-    public static String postgresScheme;
-
     public static String esUris;
     public static String esUsername;
     public static String esPassword;
+
+    public static String dataSourceDriver;
+    public static String dataSourceUrl;
+    public static String dataSourceUsername;
+    public static String dataSourcePassword;
 
 
     @Resource
@@ -40,14 +40,14 @@ public class SimpleProtoConfig {
         dataTable = environment.getProperty("simpleproto.dataTable");
         settingsTable = environment.getProperty("simpleproto.settingsTable");
 
-        postgresHost = environment.getProperty("simpleproto.postgresHost");
-        postgresPort = environment.getProperty("simpleproto.postgresPort");
-        postgresDatabase = environment.getProperty("spring.application.name");
-        postgresScheme = environment.getProperty("spring.profiles.active");
-
         esUris = environment.getProperty("simpleproto.esUris");
         esUsername = environment.getProperty("simpleproto.esUsername");
         esPassword = environment.getProperty("simpleproto.esPassword");
+
+        dataSourceDriver = environment.getProperty("simpleproto.dataSourceDriver");
+        dataSourceUrl = environment.getProperty("simpleproto.dataSourceUrl");
+        dataSourceUsername = environment.getProperty("simpleproto.dataSourceUsername");
+        dataSourcePassword = environment.getProperty("simpleproto.dataSourcePassword");
     }
 
 }
