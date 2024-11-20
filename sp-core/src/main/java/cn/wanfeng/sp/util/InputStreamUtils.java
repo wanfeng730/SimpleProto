bf50fa6aea8191a9407930e96a8560c69ddcd918
wanfeng730 <2580330397@qq.com>
2024-11-20 15:00:33 +0800

    feat: 流工具类 1.关闭流 2.从文件中获取ByteArrayInputStream

---
+++ sp-core/src/main/java/cn/wanfeng/sp/util/InputStreamUtils.java
@@ -0,0 +1,60 @@
+package cn.wanfeng.sp.util;
+
+import cn.wanfeng.proto.util.LogUtils;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * @date: 2024-11-20 14:55
+ * @author: luozh.wanfeng
+ * @description: 流工具
+ * @since: 1.0
+ */
+public class InputStreamUtils {
+
+    /**
+     * 优雅关闭流
+     * @param inputStream 流
+     */
+    public static void closeQuietly(InputStream inputStream) {
+        if (Objects.isNull(inputStream)) {
+            return;
+        }
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            LogUtils.error("InputStream.close 失败");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 从文件中获取ByteArrayInputStream流
+     *
+     * @param file 文件
+     * @return ByteArrayInputStream
+     */
+    public static ByteArrayInputStream getByteArrayInputStreamFromFile(File file) {
+        ByteArrayInputStream byteArrayInputStream;
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            byte[] fileBytes = byteArrayOutputStream.toByteArray();
+            LogUtils.debug("InputStreamUtils.getByteArrayInputStreamFromFile[fileBytes.length={}]", fileBytes.length);
+            byteArrayInputStream = new ByteArrayInputStream(fileBytes);
+
+            byteArrayOutputStream.close();
+        } catch (Exception e) {
+            LogUtils.error("从文件获取ByteArrayInputStream异常", e);
+            throw new RuntimeException(e);
+        }
+        return byteArrayInputStream;
+    }
+
+}
