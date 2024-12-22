ef01012b92b4a9074704f93f0da3be899315f08b
wanfeng730 <2580330397@qq.com>
2024-12-22 21:15:28 +0800

    feature: FileTag文件类型

--- sp-core/src/main/java/cn/wanfeng/sp/api/enums/FileTag.java
+++ sp-core/src/main/java/cn/wanfeng/sp/api/enums/FileTag.java
@@ -10,19 +10,23 @@ import org.apache.commons.lang3.StringUtils;
  */
 public enum FileTag {
 
-    IMAGE("image", "图片文件"),
-    TEXT("text", "文本文件"),
-    VIDEO("video", "视频文件"),
+    IMAGE("image", "图片文件", new String[]{"jpg", "jpeg", "png", "bmp"}),
+    TEXT("text", "文本文件", new String[]{"txt"}),
+    AUDIO("audio", "音频文件", new String[]{"mp3"}),
+    VIDEO("video", "视频文件", new String[]{"mp4"}),
 
-    NONE("The fileTag is Not Supported", "不支持的文件类型");
+    NONE("The fileTag is Not Supported", "不支持的文件类型", new String[]{});
 
     private String value;
 
     private String valueCN;
 
-    FileTag(String value, String valueCN) {
+    private String[] suffixes;
+
+    FileTag(String value, String valueCN, String[] suffixes) {
         this.value = value;
         this.valueCN = valueCN;
+        this.suffixes = suffixes;
     }
 
     public String getValue() {
