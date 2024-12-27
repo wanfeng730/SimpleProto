8ced0eb66611156d94cb3e9bacbb64ee5e9496b7
wanfeng730 <2580330397@qq.com>
2024-12-27 16:27:44 +0800

    feature: ApplicationContext获取bean

---
+++ sp-core/src/main/java/cn/wanfeng/sp/context/SimpleApplicationContext.java
@@ -0,0 +1,32 @@
+package cn.wanfeng.sp.context;
+
+import cn.wanfeng.sp.util.LogUtil;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @date: 2024-12-27 16:01
+ * @author: luozh.wanfeng
+ * @description: 实现spring的ApplicationContextAware。
+ *      spring实例化所有bean后会将容器上下文ApplicationContext传递给ApplicationContextAware接口的实现类中
+ *      实现ApplicationContextAware接口的bean可以通过接口方法获取到容器上下文
+ * @since:
+ */
+@Component
+public class SimpleApplicationContext implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static <T> T getBean(Class<T> clazz){
+        return context.getBean(clazz);
+    }
+
+    @Override
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+        LogUtil.info("SimpleApplicationContext 容器上下文初始化");
+    }
+}
