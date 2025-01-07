543fe656592ec4fd53e14a521ca3bb0098062b6a
wanfeng730 <2580330397@qq.com>
2025-01-07 22:59:32 +0800

    feat: 股票行情模块开发 1.初始化

---
+++ sp-stock/src/test/java/cn/wanfeng/spstock/SpStockApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wanfeng.spstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
