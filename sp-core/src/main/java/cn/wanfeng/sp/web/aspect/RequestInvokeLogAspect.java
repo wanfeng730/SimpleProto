61834eb118d8c029f7fac0fa255a95debe2ad239
wanfeng730 <2580330397@qq.com>
2025-05-18 12:33:54 +0800

    feat: 请求日志切面获取请求体

--- sp-core/src/main/java/cn/wanfeng/sp/web/aspect/RequestInvokeLogAspect.java
+++ sp-core/src/main/java/cn/wanfeng/sp/web/aspect/RequestInvokeLogAspect.java
@@ -4,15 +4,20 @@ package cn.wanfeng.sp.web.aspect;
 import cn.wanfeng.sp.util.DateUtils;
 import cn.wanfeng.sp.util.PrintColorUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.lang.annotation.Annotation;
 import java.util.Objects;
 
 /**
@@ -45,6 +50,7 @@ public class RequestInvokeLogAspect {
         String uri = request.getRequestURI();
         String queryString = request.getQueryString();
         String method = request.getMethod();
+        Object requestBody = getRequestBodyObject(joinPoint);
 
         String message = method + " " + uri;
         if (StringUtils.isNotBlank(queryString)) {
@@ -59,5 +65,30 @@ public class RequestInvokeLogAspect {
             return null;
         }
         return servletRequestAttributes.getRequest();
+    }
+
+    private static Object getRequestBodyObject(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        // 获取方法参数上的注解
+        Annotation[][] parameterAnnotations = signature.getMethod().getParameterAnnotations();
+        if (ArrayUtils.isEmpty(parameterAnnotations)) {
+            return null;
+        }
+        for (int i = 0; i < parameterAnnotations.length; i++) {
+            Annotation[] annotationsOnOneParameter = parameterAnnotations[i];
+            if (ArrayUtils.isEmpty(annotationsOnOneParameter)) {
+                continue;
+            }
+            for (Annotation annotation : annotationsOnOneParameter) {
+                if (annotation == null) {
+                    continue;
+                }
+                Class<? extends Annotation> annotationType = annotation.annotationType();
+                if (ClassUtils.isAssignable(annotationType, RequestBody.class)) {
+                    return joinPoint.getArgs()[i];
+                }
+            }
+        }
+        return null;
     }
 }
