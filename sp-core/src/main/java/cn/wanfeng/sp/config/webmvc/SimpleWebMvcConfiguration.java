d37d25056bb0b48796b226e3554c4d769b4b0d57
wanfeng730 <2580330397@qq.com>
2025-06-02 23:19:35 +0800

    feat: 升版1.10 解决swagger与序列化类型自定义冲突

--- sp-core/src/main/java/cn/wanfeng/sp/config/webmvc/SimpleWebMvcConfiguration.java
+++ sp-core/src/main/java/cn/wanfeng/sp/config/webmvc/SimpleWebMvcConfiguration.java
@@ -2,10 +2,19 @@ package cn.wanfeng.sp.config.webmvc;
 
 
 import cn.wanfeng.sp.util.LogUtil;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * SimpleWebMvcConfiguration: desc.
@@ -41,5 +50,24 @@ public class SimpleWebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         LogUtil.info("[SimpleProto] WebMvc加载Swagger前端资源");
         super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = converter.getObjectMapper();
+        // 生成JSON时,将所有Long转换成String
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        // 时间格式化
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        // 设置格式化内容
+        converter.setObjectMapper(objectMapper);
+
+        // 转换器放在第二个位置，保证swagger正常访问，时间格式可以正常转换
+        converters.add(1, converter);
     }
 }
