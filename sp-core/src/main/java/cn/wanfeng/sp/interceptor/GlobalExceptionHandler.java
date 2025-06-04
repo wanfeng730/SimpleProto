b3efaad00f5108e49db371441edebd973d988f2a
wanfeng730 <2580330397@qq.com>
2025-06-04 22:58:55 +0800

    feat: 升版1.14 限定异常处理的包，不影响其他项目

--- sp-core/src/main/java/cn/wanfeng/sp/interceptor/GlobalExceptionHandler.java
+++ sp-core/src/main/java/cn/wanfeng/sp/interceptor/GlobalExceptionHandler.java
@@ -4,8 +4,6 @@ package cn.wanfeng.sp.interceptor;
 import cn.wanfeng.sp.exception.SimpleExceptionCode;
 import cn.wanfeng.sp.exception.SpException;
 import cn.wanfeng.sp.model.ResponseEntity;
-import jakarta.annotation.Resource;
-import org.springframework.context.MessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,11 +14,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @date: 2025-05-01 23:54
  * @author: luozh.wanfeng
  */
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "cn.wanfeng.sp.web.controller")
 public class GlobalExceptionHandler implements Ordered {
-
-    @Resource
-    private MessageSource messageSource;
 
     /**
      * 异常拦截处理
@@ -37,25 +32,13 @@ public class GlobalExceptionHandler implements Ordered {
     }
 
     /**
-     * 处理国际化信息
+     * 异常信息格式化
      * @param message 信息
      * @param args 填充参数
      * @return 处理后的信息
      */
     private String handleMessageFormat(String message, Object... args) {
         return String.format(message, args);
-
-        // 处理国际化信息方式 使用i18n
-        // try {
-        //     String finalMessage = messageSource.getMessage(message, null, LocaleContextHolder.getLocale());
-        //     if(ArrayUtils.isNotEmpty(args)){
-        //         finalMessage = String.format(finalMessage, args);
-        //     }
-        //     return finalMessage;
-        // } catch (NoSuchMessageException e) {
-        //     LogUtil.error("No Message For Exception Code: {}. Please Check file messages.properties", message);
-        //     return message;
-        // }
     }
 
     @Override
