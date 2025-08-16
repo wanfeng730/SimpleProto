04117d48fbdfb1fc65c680d904affc6a76375072
wanfeng730 <2580330397@qq.com>
2025-08-16 17:18:39 +0800

    feat: 获取拼音方法

--- sp-common/src/main/java/cn/wanfeng/sp/util/ChineseUtil.java
+++ sp-common/src/main/java/cn/wanfeng/sp/util/ChineseUtil.java
@@ -1,6 +1,7 @@
 package cn.wanfeng.sp.util;
 
 
+import com.github.promeg.pinyinhelper.Pinyin;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.regex.Matcher;
@@ -30,6 +31,42 @@ public class ChineseUtil {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 获取全拼大写
+     *
+     * @param chinese 中文
+     * @return 全拼
+     */
+    public static String getPinyin(String chinese) {
+        return getPinyin(chinese, ""); // 第二个参数是分隔符（空字符串表示无分隔）
+    }
+
+    /**
+     * 获取全拼大写
+     *
+     * @param chinese   中文
+     * @param separator 分隔符
+     * @return 全拼
+     */
+    public static String getPinyin(String chinese, String separator) {
+        return Pinyin.toPinyin(chinese, separator);
+    }
+
+
+    // 获取首字母大写
+    public static String getPinyinHeader(String chinese) {
+        StringBuilder headers = new StringBuilder();
+        for (char c : chinese.toCharArray()) {
+            if (Pinyin.isChinese(c)) {
+                String pinyin = Pinyin.toPinyin(c);
+                headers.append(pinyin.charAt(0)); // 取拼音首字母
+            } else {
+                headers.append(c);
+            }
+        }
+        return headers.toString();
     }
 
 }
