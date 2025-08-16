47c6ebe7c44e0072f94220e267d86a98a7a38f8c
wanfeng730 <2580330397@qq.com>
2025-08-16 16:23:58 +0800

    feat: 请求方式

---
+++ sp-common/src/main/java/cn/wanfeng/sp/constants/RequestMethod.java
@@ -0,0 +1,18 @@
+package cn.wanfeng.sp.constants;
+
+
+/**
+ * RequestMethod: 请求方式.
+ *
+ * @date: 2025-08-09 18:41
+ * @author: luozh.wanfeng
+ */
+public class RequestMethod {
+
+    public static final String GET = "GET";
+
+    public static final String POST = "POST";
+
+    public static final String OPTIONS = "OPTIONS";
+
+}
