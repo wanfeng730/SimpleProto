06a92570cf4644864df1ebe29d537b844681831c
wanfeng730 <2580330397@qq.com>
2025-11-12 15:40:10 +0800

    feat：double格式化转string方法

--- sp-common/src/main/java/cn/wanfeng/sp/util/NumberUtils.java
+++ sp-common/src/main/java/cn/wanfeng/sp/util/NumberUtils.java
@@ -1,6 +1,10 @@
 package cn.wanfeng.sp.util;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
 /**
  * @date: 2024-12-15 18:19
  * @author: luozh.wanfeng
@@ -14,4 +18,28 @@ public class NumberUtils {
     }
 
 
+    /**
+     * double转string
+     * @param value 值
+     * @param scale 保留小数位数
+     * @param roundingMode 取舍枚举
+     * @return string
+     */
+    public static String doubleToString(Double value, int scale, RoundingMode roundingMode){
+        if(Objects.isNull(value)){
+            return null;
+        }
+        BigDecimal decimal = new BigDecimal(value);
+        decimal = decimal.setScale(scale, roundingMode);
+        return decimal.toString();
+    }
+
+    /**
+     * double转string，保留两位小数、四舍五入
+     * @param value 值
+     * @return string
+     */
+    public static String doubleToString(Double value){
+        return doubleToString(value, 2, RoundingMode.HALF_UP);
+    }
 }
