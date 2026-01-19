d355d937ac14340a756827e6c162e4e7ca12748b
wanfeng730 <2580330397@qq.com>
2026-01-19 18:02:51 +0800

    feat: 取平均值

--- sp-common/src/main/java/cn/wanfeng/sp/util/DoubleUtil.java
+++ sp-common/src/main/java/cn/wanfeng/sp/util/DoubleUtil.java
@@ -1,7 +1,12 @@
 package cn.wanfeng.sp.util;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * DoubleUtil: desc.
@@ -27,7 +32,7 @@ public class DoubleUtil {
      * @param d1 分子
      * @param d2 分母
      * @param scale 保留小数位数
-     * @return String
+     * @return Double
      */
     public static Double divide(Double d1, Double d2, int scale){
         return divide(d1, d2, scale, RoundingMode.HALF_UP);
@@ -39,11 +44,30 @@ public class DoubleUtil {
      * @param d2 分母
      * @param scale 保留小数位数
      * @param roundingMode 保留模式
-     * @return String
+     * @return Double
      */
     public static Double divide(Double d1, Double d2, int scale, RoundingMode roundingMode){
         BigDecimal b1 = BigDecimal.valueOf(d1);
         BigDecimal b2 = BigDecimal.valueOf(d2);
         return b1.divide(b2, scale, roundingMode).doubleValue();
+    }
+
+    /**
+     * 取平均值 四舍五入
+     * @param scale 保留小数位数
+     * @param values 数值
+     * @return Double
+     */
+    public static Double average(int scale, Double... values){
+        List<Double> validValues = Arrays.stream(values).filter(Objects::nonNull).toList();
+        if(CollectionUtils.isEmpty(validValues)){
+            return null;
+        }
+        BigDecimal count = new BigDecimal(values.length);
+        BigDecimal sum = new BigDecimal(0);
+        for (Double value : validValues) {
+            sum = sum.add(BigDecimal.valueOf(value));
+        }
+        return sum.divide(count, scale, RoundingMode.HALF_UP).doubleValue();
     }
 }
