3925e4017cdeca52fe6f647d5e51e5050527ec2d
wanfeng730 <2580330397@qq.com>
2026-02-08 15:32:26 +0800

    pref：增加bulkStore方法传参类型

--- sp-core/src/main/java/cn/wanfeng/sp/session/SpBulkOperator.java
+++ sp-core/src/main/java/cn/wanfeng/sp/session/SpBulkOperator.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -67,6 +68,10 @@ public class SpBulkOperator {
             bulkUpdateObject(updateList);
         }
 
+    }
+
+    public void bulkStore(ISpBaseObject... baseObjects){
+        bulkStore(Arrays.stream(baseObjects).toList());
     }
 
     public void bulkRemove(List<? extends ISpBaseObject> objectList){
